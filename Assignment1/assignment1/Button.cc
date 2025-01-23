842c8b3c44f519c8984ef60967a49444853ee1c1
Assi Assi <assia8632@gmail.com>
2025-01-23 11:59:06 -0500

    Working on button class

---
+++ Assignment1/assignment1/Button.cc
@@ -0,0 +1,36 @@
+#include "Button.h"
+#include "defs.h"
+#include <iostream>
+
+using namespace std;
+
+
+Button::Button(int x, int y, int width, int height, string label, RGB fill, RGB border){
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
+    this->label = label;
+    this->fill = fill;
+    this->border = border;
+};
+
+Button::Button(int x, int y, int width, int height, string label, CuColour fill, CuColour border){
+    this->x = x;
+    this->y = y;
+    this->width = width;
+    this->height = height;
+    this->label = label;
+    this->fill = RGB(fill);
+    this->border = RGB(border);
+};
+
+Button::Button(){
+    this->x = 0;
+    this->y = 0;
+    this->width = 0;
+    this->height = 0;
+    this->label = "";
+    this->fill = RGB(112,128,144);
+    this->border = RGB(77,117,154);
+};
--- Assignment1/assignment1/Button.h
+++ Assignment1/assignment1/Button.h
@@ -1,12 +1,41 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 #include <string>
+#include "RGB.h"
+#include "defs.h"
+
 using namespace std;
+
 
 class Button {
 
     public:
+        Button(int x, int y, int width, int height, string label, RGB fill, RGB border);
+        Button(int x, int y, int width, int height, string label, CuColour fill, CuColour border);
+        Button();
 
+        int getX();
+        int getY();
+        int getWidth();
+        int getHeight();
+        string getLabel();
+        RGB getFill();
+        RGB getBorder();
+        CuColour getFillCu();
+        CuColour getBorderColour();
+
+        void setX(int x);
+        void setY(int y);
+        void setWidth(int width);
+        void setHeight(int height);
+        void setLabel(string label);
+        void setFill(RGB fill);
+        void setBorder(RGB border);
+        void setFill(CuColour fill);
+        void setBorder(CuColour border);
+
+        void draw(Display *display, Window win, GC gc, int parentX, int parentY);
+        bool overlaps()
 
     private:
         int x;
@@ -14,6 +43,7 @@ class Button {
         int width;
         int height;
         string label;
-
+        RGB fill;
+        RGB border;
 };
 #endif
