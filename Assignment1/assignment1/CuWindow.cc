4520563234aae1543395312feae821506bbf8ed1
Assi Assi <assia8632@gmail.com>
2025-01-25 17:22:55 -0500

    leaving now see you later

--- Assignment1/assignment1/CuWindow.cc
+++ Assignment1/assignment1/CuWindow.cc
@@ -3,6 +3,40 @@
 #include <X11/Xlib.h>
 #include <unistd.h>
 #include <string>
+#include "defs.h"
+#include "RGB.h"
+
 
 using namespace std;
+
+bool CuWindow::addPanel(Panel& p){
+    for (int i = 0; i <numOfPanels; i++){
+        if (panels[i].overlaps(p) && p.getX() + p.getWidth() > width || p.getY() + p.getHeight() > height){
+            return false;
+        };
+    };
+    panels[numOfPanels] = p;
+    numOfPanels++;
+    return true;
+};
+
+bool CuWindow::removePanel(string id){
+    for (int i = 0; i < numOfPanels; i++){
+        if (panels[i].getId() == id){
+            for (int j = i; j < numOfPanels; j++){
+                panels[j] = panels[j+1];
+            };
+            numOfPanels--;
+            return true;
+        }
+    }
+};
+
+Panel* CuWindow::getPanel(string id){
+    for (int i = 0; i <numOfPanels; i++){
+        if (panels[i].getId() == id){
+            return &panels[i];
+        };
+    return nullptr;
+};
 
