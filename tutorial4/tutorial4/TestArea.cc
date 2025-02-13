89b170fd0060d5e6249dace4189d310c3ac6f435
Assi Assi <assia8632@gmail.com>
2025-02-13 12:57:20 -0500

    working on member functions now

--- tutorial4/tutorial4/TestArea.cc
+++ tutorial4/tutorial4/TestArea.cc
@@ -1,1 +1,13 @@
 #include "TextArea.h"
+#include "RGB.h"
+#include "defs.h"
+
+TextArea::TextArea(int x, int y, int width, int height, string id, RGB fill, RGB border){
+    this->dimensions = Rectangle({x, y, width, height});
+    this->id = id;
+};
+TextArea::TextArea(Rectangle &rect, string id, string lable, RGB fill, RGB border){
+    this->dimensions = rect;
+    this->id = id;
+}
+
--- tutorial4/tutorial4/TextArea.h
+++ tutorial4/tutorial4/TextArea.h
@@ -1,14 +1,20 @@
 #ifndef TEXTAREA_H
 #define TEXTAREA_H
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class TextArea{
     public:
-
+    TextArea(int x, int y, int width, int height, string id, RGB fill = RGB::WHITE(), RGB border = RGB::BLACK());
+    TextArea(Rectangle &rect, string id, string lable, RGB fill = RGB::WHITE(), RGB border = RGB::BLACK());
 
     private:
-    
-
-
-
-}
-#endif;
+        Rectangle dimensions;
+        string text;
+        string id;
+        RGB fill;
+        RGB border;
+};
+#endif
