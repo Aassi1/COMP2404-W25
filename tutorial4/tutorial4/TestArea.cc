ccc0eb9c097d2a0125c964aa7374754e6389e3db
Assi Assi <assia8632@gmail.com>
2025-02-13 13:12:49 -0500

    finished everything but draw function

--- tutorial4/tutorial4/TestArea.cc
+++ tutorial4/tutorial4/TestArea.cc
@@ -2,6 +2,10 @@
 #include "RGB.h"
 #include "defs.h"
 
+#include <iostream>
+using namespace std;
+
+
 TextArea::TextArea(int x, int y, int width, int height, string id, RGB fill, RGB border){
     this->dimensions = Rectangle({x, y, width, height});
     this->id = id;
@@ -11,3 +15,19 @@ TextArea::TextArea(Rectangle &rect, string id, string lable, RGB fill, RGB borde
     this->id = id;
 }
 
+// void TextArea::draw(Display *display, Window win, GC gc, int x, int y){
+//     XSetForeground(display, gc, fill.getColour());
+//     XFillRectangle(display, win, gc, x, y, dimensions.getWidth(), dimensions.getHeight());
+//     XSetForeground(display, gc, border.getColour());
+//     XDrawRectangle(display, win, gc, x, y, dimensions.getWidth(), dimensions.getHeight());
+// }
+bool TextArea::overlaps(TextArea &textArea){
+    return dimensions.overlaps(textArea.dimensions);
+}
+
+void TextArea::print(){
+    cout << "TextArea id: " << id << endl;
+    cout << "Preferred location: " << dimensions.x << ", " << dimensions.y << endl;
+    cout << "Size: " << dimensions.width<< ", " << dimensions.height << endl;
+    cout << "Text: " << text << endl;
+}
--- tutorial4/tutorial4/TextArea.h
+++ tutorial4/tutorial4/TextArea.h
@@ -3,12 +3,21 @@
 
 #include <iostream>
 #include <string>
+#include <X11/Xlib.h>
+
+
 using namespace std;
 
 class TextArea{
     public:
     TextArea(int x, int y, int width, int height, string id, RGB fill = RGB::WHITE(), RGB border = RGB::BLACK());
     TextArea(Rectangle &rect, string id, string lable, RGB fill = RGB::WHITE(), RGB border = RGB::BLACK());
+    TextArea();
+    
+    void draw(Display *display, Window win, GC gc, int x, int y);
+    bool overlaps(TextArea &textArea);
+
+    void print();
 
     private:
         Rectangle dimensions;
