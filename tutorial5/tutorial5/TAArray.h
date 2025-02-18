085c7152d48544eade93daa59b339605960e304f
Assi Assi <assia8632@gmail.com>
2025-02-17 23:15:12 -0500

    working on tut5

--- tutorial5/tutorial5/TAArray.h
+++ tutorial5/tutorial5/TAArray.h
@@ -11,12 +11,17 @@ class TAArray{
 
         bool add(TextArea &tas);
         bool add(TextArea &ta, int index);
+        TextArea* get(int index) const;
+        TextArea* get(std::string id) const;
+        TextArea* remove(int index);
+        TextArea* remove(std::string id);
+        int getSize() const;
 
     private:
         TextArea **areas;
         int size;
         int capacity;
-        
+
 
 };
 #endif
