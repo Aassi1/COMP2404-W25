9d7ad44b7addd708036a48a17cd88f2059903b4d
Assi Assi <assia8632@gmail.com>
2025-02-22 12:59:17 -0500

    finished FlowPanel.h

--- Assignment2/assignment2/FlowPanel.h
+++ Assignment2/assignment2/FlowPanel.h
@@ -3,23 +3,33 @@
 
 #include "TextArea.h"
 #include "TAArray.h"
+#include "X11/Xlib.h"
 #include "defs.h"
 #include <string>
 
 using namespace std;
 
-// struct Rectangle {
-//     int x, y, width, height;
-// };
-
 class FlowPanel {
     public :
         FlowPanel(int x, int y, int width, int height, string id, int xgap, int ygap);
         FlowPanel(Rectangle rectangle, string id, int xgap, int ygap);
+        FlowPanel(FlowPanel &other); 
+        ~FlowPanel();
+
+        bool addTextArea(TextArea& ta);
+        bool addTextArea(TextArea& ta, int index);
+        TextArea getTextArea(string id);
+        TextArea getTextArea(int index);
+        TextArea removeTextArea(string id);
+        TextArea removeTextArea(int index);
+
+
+        bool overlaps(FlowPanel &other);
+        void draw(Display *display, Window win, GC gc);
 
 
         void print() const;
-        void printTextArea();
+        void printTextArea() const;
 
     private :
         string id;
@@ -29,44 +39,3 @@ class FlowPanel {
 
 };
 #endif
-
-// #ifndef FLOWPANEL_H
-// #define FLOWPANEL_H
-
-// #include "defs.h"
-// #include "TextArea.h"
-// #include "TAArray.h"
-// #include <string>
-
-
-// class FlowPanel {
-//     public:
-//         // Constructors
-//         FlowPanel(int x, int y, int width, int height, std::string id, int xgap = 10, int ygap = 10);
-//         FlowPanel(Rectangle rectangle, std::string id, int xgap = 10, int ygap = 10);
-//         FlowPanel(const FlowPanel& other); // Copy constructor
-
-//         // Destructor
-//         ~FlowPanel();
-
-//         // Add a TextArea to the FlowPanel (transfers ownership)
-//         void addTextArea(TextArea* textArea);
-
-//         // Get a TextArea by index (does not transfer ownership)
-//         TextArea* getTextArea(int index) const;
-
-//         // Remove a TextArea by index (transfers ownership to the caller)
-//         TextArea* removeTextArea(int index);
-
-//         // Print functions
-//         void print() const;
-//         void printTextArea() const;
-
-//     private:
-//         std::string id;          // Unique ID of the FlowPanel
-//         Rectangle dimensions;    // Position and size of the FlowPanel
-//         int xgap, ygap;          // Gaps between TextAreas
-//         TAArray areas;           // Array to store TextAreas
-// };
-
-// #endif
