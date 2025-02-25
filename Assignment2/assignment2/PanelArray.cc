d676f731d937233b76a25bda2ead75b0002ef97e
Assi Assi <assia8632@gmail.com>
2025-02-25 01:15:06 -0500

    PanelArray.cc done

--- Assignment2/assignment2/PanelArray.cc
+++ Assignment2/assignment2/PanelArray.cc
@@ -1,21 +1,36 @@
 #include "PanelArray.h"
 
-PanelArray::~PanelArray(){
+PanelArray::PanelArray() {
+    flowPanel = new FlowPanel*[10];
+    size = 0;
+    capacity = 10;
+}
+
+PanelArray::~PanelArray() {
     delete[] flowPanel;
 }
 
-bool PanelArray::add(FlowPanel* panel){
-
-    if (size >= capacity ){
-        FlowPanel** newArray = new FlowPanel*[capacity * 2];
-        for (int i = 0; i < size; ++i) {
-            newArray[i] = flowPanel[i];
-        }
-        delete[] flowPanel;
-        flowPanel = newArray;
-        capacity *= 2;
-    }
-    flowPanel[size] = panel;
-    size++;
+bool PanelArray::add(FlowPanel* panel) {
+    if (size >= capacity) return false;
+    flowPanel[size++] = panel;
     return true;
+}
+
+FlowPanel* PanelArray::get(int index) {
+    if (index < 0 || index >= size) return nullptr;
+    return flowPanel[index];
+}
+
+int PanelArray::getSize() const {
+    return size;
+}
+
+FlowPanel* PanelArray::remove(int index) {
+    if (index < 0 || index >= size) return nullptr;
+    FlowPanel* panel = flowPanel[index];
+    for (int i = index; i < size - 1; i++) {
+        flowPanel[i] = flowPanel[i + 1];
+    }
+    size--;
+    return panel;
 }
